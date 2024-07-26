57999ae8b2cbb6b5d571a0b3ad2a1474c82d9147
Yiding Chang <colossuschang@sjtu.edu.cn>
2024-07-25 22:46:22 -0700

    Fix naturalReaders

--- scripts/voiceGetters/naturalReaders.py
+++ scripts/voiceGetters/naturalReaders.py
@@ -23,7 +23,7 @@ def getSoundForLine(words: str, number: str, voice):
     display_name = voice["voice"]
     locale = voice["locale"]
     # voice_id is only present if the voice is cloned
-    if voice["voice_id"] is not None:
+    if "voice_id" in voice:
         url = f"https://l6m5prrx81.execute-api.us-east-1.amazonaws.com/prod220818/el/speak?display_name={display_name}&speed=180&style=&locale={locale}&model=v2&voice_id={voice['voice_id']}"
     else:
         url = f"https://l6m5prrx81.execute-api.us-east-1.amazonaws.com/prod220818/el/speak?display_name={display_name}&speed=180&style=&locale={locale}&model=v2"
